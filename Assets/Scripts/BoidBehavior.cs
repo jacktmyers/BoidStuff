033965f062c3eeaadf4ddc60d951fb299510cd14
Jack Myers <jthomasmmail@gmail.com>
2024-08-19 21:40:49 -0400

    WIP

--- Assets/Scripts/BoidBehavior.cs
+++ Assets/Scripts/BoidBehavior.cs
@@ -1,18 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BoidBehavior : MonoBehaviour
 {
+    private Rigidbody2D rigidBody;
+    public bool initialized {get; private set;} = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+        initialized = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public Vector2 Get2DPos(){
+        if (!initialized){
+            return Vector2.zero;
+        }
+        return new Vector2(this.gameObject.transform.position.x,this.gameObject.transform.position.y);
+    }
+    public Vector2 Get2DVel(){
+        if (!initialized){
+            return Vector2.zero;
+        }
+        return rigidBody.velocity;
+    }
+    public void DeltaVelocity(Vector2 delta){
+        if (!initialized){
+            return;
+        }
+        this.rigidBody.velocity += delta;
+    }
+    public void ReactiveForces(BoidBehavior[] boids, float protectedRange, float visibleRange, float sepFactor, float alignFactor){
+        BoidBehavior[] protectedBoids = boids.Where(b => Vector2.Distance(b.Get2DPos(), this.Get2DPos()) < protectedRange).ToArray();
+        BoidBehavior[] visibleBoids = boids.Where(b => Vector2.Distance(b.Get2DPos(), this.Get2DPos()) < visibleRange && !protectedBoids.Contains(b)).ToArray();
+
+        // Separation
+        Vector2 sep = Vector2.zero;
+        foreach(BoidBehavior currBoid in protectedBoids){
+            sep += this.Get2DPos() - currBoid.Get2DPos();
+        }
+        this.DeltaVelocity(sep*sepFactor);
+
+        // Alignment
+        Vector2 velAvg = Vector2.zero;
+        int visibleCount = 0;
+        foreach(BoidBehavior currBoid in visibleBoids){
+            velAvg += currBoid.Get2DVel();
+            visibleCount++;
+        }
+        if (visibleCount > 0){
+            velAvg = velAvg / visibleCount;
+            this.DeltaVelocity((velAvg - this.Get2DVel())*alignFactor);
+        }
     }
 }
--- Assets/Scripts/BoidManager.cs
+++ Assets/Scripts/BoidManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BoidManager : MonoBehaviour
 {
+    private List<BoidBehavior> AllBoids;
+    [Min(1)]
+    public int BoidCount = 1;
+    public BoidBehavior BoidPrefab;
+    public Vector2 YSpawnRange;
+    public Vector2 XSpawnRange;
+    public float ProtectedRange;
+    public float VisibleRange;
+    public float SeparationFactor;
+    public float AlignFactor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AllBoids = new List<BoidBehavior>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (AllBoids.Count < BoidCount){
+            for (int i=0; i<BoidCount-AllBoids.Count; i++){
+                BoidBehavior newBoid = Instantiate(BoidPrefab);
+                newBoid.gameObject.transform.position = new Vector2(Random.Range(XSpawnRange.x,XSpawnRange.y),Random.Range(YSpawnRange.x,YSpawnRange.y));
+                AllBoids.Add(newBoid);
+            }
+        }
+        if (AllBoids.Count > BoidCount){
+            for (int i=0; i<AllBoids.Count-BoidCount; i++){
+                BoidBehavior removedBoid = AllBoids.Last();
+                AllBoids.Remove(removedBoid);
+                Destroy(removedBoid.gameObject);
+            }
+        }
+        foreach (BoidBehavior currBoid in AllBoids){
+            currBoid.ReactiveForces(AllBoids.ToArray(), ProtectedRange, VisibleRange, SeparationFactor, AlignFactor);
+        }
         
     }
 }
